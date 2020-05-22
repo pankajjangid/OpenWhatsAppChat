ab94dcd6b10100f723cf687d47d64cd1e33c8e11
Pankaj Jangid <pankaj0619@gmail.com>
2020-05-22 20:50:55 +0530

    Initial commit

---
+++ app/src/main/java/com/whatsappdirect/direct_chat/Utils/AdapterCallback.java
@@ -0,0 +1,11 @@
+package com.whatsappdirect.direct_chat.Utils;
+
+/**
+ * Created by iblinfotech on 13/09/18.
+ */
+
+public interface AdapterCallback {
+
+    public void onPositionClicked(String phoneNUmber);
+
+}
