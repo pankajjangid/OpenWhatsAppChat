7eec2aa645d28625bb4a3b13d8bc34113b04efac
Pankaj Kumar Jangid <pankaj0619@gmail.com>
2025-12-31 19:29:16 +0800

    Uploaded on playstore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.whatsappdirect.direct_cha"
         minSdk = 24
         targetSdk = 35
-        versionCode = 21
-        versionName = "3.3"
+        versionCode = 22
+        versionName = "3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
